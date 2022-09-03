77fede012cf24b148db4754fd9ddba58b4832ebe
EgorEast <egoreast.true@gmail.com>
2022-09-04 01:05:08 +0300

    добавил роутинг и переключение темы

--- frontend/src/components/Events/Events.tsx
+++ frontend/src/components/Events/Events.tsx
@@ -1,11 +1,17 @@
-﻿import {useSelector} from "react-redux";
-import {selectEvents} from "../../redux/events.slice";
+﻿import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { selectEvents } from 'redux/events.slice';
 
-export function Events() {
+export default function Events() {
     const events = useSelector(selectEvents);
-    return <div>
-        {events.map(e => 
-            <div>{e.title}</div> 
-        )}
-    </div>;
+    return (
+        <div>
+            {events.map((e) => (
+                <div>{e.title}</div>
+            ))}
+            <div>
+                <Link to='empty'>Go to empty</Link>
+            </div>
+        </div>
+    );
 }
